e147e1096f02fb2c621bbb1ce3804c44192e4e5f
Md. Abu Taher <entrptaher@users.noreply.github.com>
2018-02-03 03:28:10 +0600

    Update mobx.js

--- mobx.js
+++ mobx.js
@@ -2,8 +2,16 @@ import React from "react";
 import { render } from "react-dom";
 import { observable, action } from "mobx";
 import { observer } from "mobx-react";
+import autobind from "react-autobind";
 
+// prepare our store (to store the data)
 class Store {
+  constructor() {
+    // no more arrow functions everywhere
+    autobind(this);
+  }
+
+  // look out for title to be updated
   @observable title = "Hello Mobx";
 
   @action
@@ -12,24 +20,28 @@ class Store {
   }
 }
 
+// observe the title for change (if needed)
 const Title = observer(props => {
   return <div>{store.title}</div>;
 });
 
+// observe the form for change
 const Form = observer(props => {
   return (
     <div>
-      <input value={store.title} onChange={store.updateTitle} />
+      <input 
+        value={store.title} 
+        onChange={store.updateTitle} 
+      />
     </div>
   );
 });
 
-@observer
-class App extends React.Component {
-  render() {
-    return [<Title />, <Form />];
-  }
-}
+// mashup the components
+const App = observer(() => [<Title />, <Form />]);
 
+// create store
 const store = new Store();
+
+// render everything
 render(<App />, document.getElementById("root"));
